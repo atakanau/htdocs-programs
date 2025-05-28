c50e29fb1b60392ca3ef35e5120bc13cd2371b55
Atakan Au <altun.atakan@gmail.com>
2025-05-28 19:49:23 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+{
+	"version": "1.0.0",
+	"verification": "68747470733A2F2F6765726865732E70726F6772616D732E636F6D2E7472"
+}
+*/
+document.querySelectorAll('a[href^="mailto:"]').forEach(link => {
+	link.href = link.href.replace('|', '@');
+});
